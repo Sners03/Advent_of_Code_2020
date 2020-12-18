6f2fff15186b28d9f7086d2f3e09a7472235d745
Sners03 <reichelarslukas@gmail.com>
2020-12-18 22:30:30 +0100

    day 1 silver 1

---
+++ Day_1/silver_code.py
@@ -0,0 +1,18 @@
+from tkinter import filedialog
+
+def get_txt_content():
+    filename =  filedialog.askopenfilename(initialdir = "",
+                                            title = "Select file",
+                                            filetypes = (("txt files","*.txt")
+                                                        ,("all files","*.*")))
+    file = open(filename,'r')
+    content = file.readlines()
+    file.close()
+
+    input_arr = []
+
+    for num in content:
+        input_arr.append(int(num))
+    return input_arr
+
+print(get_txt_content())
