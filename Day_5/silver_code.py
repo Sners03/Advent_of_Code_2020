48eabb1f201cf0c0dbf60d4b986f60c94f39b524
Sners03 <reichelarslukas@gmail.com>
2021-03-06 16:30:07 +0100

    added first half of get_seat_ID row can now be searched

--- Day_5/silver_code.py
+++ Day_5/silver_code.py
@@ -16,3 +16,19 @@ def get_txt_content():
 
     return input_arr
 
+def get_seat_ID(pass_code):
+    rownum = 0
+    columnnum = 0
+    lower_row_marker = 0
+    higher_row_marker = 127
+    lower_column_marker = 0
+    higher_column_marker = 127
+
+    for i, char in enumerate(pass_code[:-3]):
+        if(char=='F'):
+            higher_row_marker -= (64/(i+1))
+        if(char=='B'):
+            lower_row_marker += (64/(i+1))
+    
+    return higher_row_marker*8
+
